b42d188c1616460c3f0ffde50fc32fef5c2893fc
Ihor <igornepipenko@gmail.com>
2020-05-21 20:19:54 +0300

    Feature: View/Content child [ref:no-ref]

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent implements AfterContentInit, AfterViewInit {
 
-  constructor() { }
+  @ContentChild('customContent', {read: TemplateRef})
+  public contentBlock!: TemplateRef<HTMLElement>;
 
-  ngOnInit(): void {
+  @ViewChild('contentPlace', {read: ViewContainerRef})
+  public contentPlace!: ViewContainerRef;
+
+  ngAfterViewInit(): void {
+    console.log(this.contentPlace);
+    this.contentPlace.createEmbeddedView(this.contentBlock);
+  }
+
+  ngAfterContentInit(): void {
+    console.log(this.contentBlock);
   }
 
 }
