1a67130b814cc6cb4cae00f1630ae319d9287ff7
Ihor <igornepipenko@gmail.com>
2020-06-25 21:06:48 +0300

    Feature: finish with change detection [ref:no-ref]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Location } from '@angular/common';
 
 // ReactiveX = Observer + Iterator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   constructor(
     private location: Location,
+   //  private ngZone: NgZone,
+    // private appRef: ApplicationRef,
+    // private chr: ChangeDetectorRef,
   ) {
+   // chr.reattach();
+    // setTimeout(() => {
+    //   console.log('TICK');
+    //   this.appRef.tick();
+    // }, 7000);
+    // vk.get((user)=>{
+    //   this.ngZone.runOutsideAngular(()=>{
+    //     this.user = user;
+    //   })
+    // })
   }
 
   back() {
